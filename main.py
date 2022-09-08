cff43e1b35c3727556ef3f371302bcaaa3986bc4
ultraacc <hade04yt@gmail.com>
2022-09-08 17:09:28 +0200

    limit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ def main():
     b = True
     while True:
         message = input("Message: ")
+        if(len(message) > 144):
+            print("Your message reached the limit of 144 characters")
         client.send_message("/chatbox/input", [message.encode('ascii', errors="replace").decode(), b])
     
 if __name__ == "__main__":
