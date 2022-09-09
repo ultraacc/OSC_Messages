81600940ae1d0e7782a196899e5a1c0a5e901f1a
ultraacc <hade04yt@gmail.com>
2022-09-09 11:13:08 +0200

    style

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from pythonosc import udp_client
 
+
 def main():
     client = udp_client.SimpleUDPClient("127.0.0.1", 9000)
     b = True
     while True:
         message = input("Message: ")
-        if(len(message) > 144):
-            print("Your message reached the limit of 144 characters")
-        client.send_message("/chatbox/input", [message.encode('ascii', errors="replace").decode(), b])
-    
+        if len(message) > 144:
+            print(
+                "Your message reached the limit of 144 characters, your message might be cut off"
+            )
+        client.send_message(
+            "/chatbox/input", [message.encode("ascii", errors="replace").decode(), b]
+        )
+
+
 if __name__ == "__main__":
     main()
